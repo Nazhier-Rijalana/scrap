b3efcd85541a91b6bae2ee21861e5e76c858c115
nazhier9974 <nazhier9974@gmail.com>
2019-03-26 20:41:47 +0700

    Inital Release

---
+++ app.py
@@ -0,0 +1,94 @@
+from bs4 import BeautifulSoup
+from flask import Flask
+import requests as req
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+db = SQLAlchemy()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:toor@localhost/rumahginjal'
+app.config['SECRET_KEY'] = 'p9Bv<3Eid9%$i01'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+class HalDepan(db.Model):
+    __tablename__ = 'haldepan'
+    id_ = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.TEXT)
+
+class DetailedNews(db.Model):
+    __tablename__ = 'detail_news'
+    iddetail_news = db.Column(db.Integer, primary_key=True)
+    judul = db.Column(db.TEXT)
+    content = db.Column(db.TEXT)
+    url = db.Column(db.TEXT)
+
+
+
+
+@app.route('/')
+def hello_world():
+    urls = HalDepan.query.with_entities(HalDepan.url).all()
+    base_url = 'http://rumahginjal.id'
+    url = 'http://rumahginjal.id/category/berita'
+    data = ''
+    for i in range(1,11):
+        get_data(url+'?page='+str(i), base_url)
+    for i in urls:
+        data = get_detail_berita(i.url)
+    return data
+
+def save_berita(judul, content, url):
+    data = DetailedNews(
+        judul=judul,
+        url=url,
+        content = content
+    )
+    db.session.add(data)
+    db.session.commit()
+    return True
+
+def scrapt(url):
+    requests = req.get(url).text
+    return requests
+
+
+def sorting_html(url):
+    a = scrapt(url)
+    bs = BeautifulSoup(a, 'lxml')
+    dat = bs.find('section', {'class': 'g-pt-100 g-pb-50'})
+    listartikel = dat.find_all('div', {'class': 'col-md-8 align-self-center g-pl-20'})
+    return listartikel
+
+
+def simpan_database_haldepan(url):
+    if url is not None:
+        hal = HalDepan(url=url)
+        db.session.add(hal)
+        db.session.commit()
+        return True
+    return False
+
+def get_data(url, base_url):
+    data = sorting_html(url)
+    # arr_url = []
+    for i in range(len(data)):
+        get_detail_berita(base_url + data[i].a['href'])
+        simpan_database_haldepan(base_url + data[i].a['href'])
+    return "Selesai"
+
+
+def get_detail_berita(url):
+    mentah = scrapt(url)
+    bs = BeautifulSoup(mentah, 'lxml')
+    artikel = bs.find('div', {'class': 'g-font-size-16 g-line-height-1_8 g-mb-30'})
+    artikel1 = artikel.find_all('p')
+    judul = bs.find('h2', {'class': 'h1 g-mb-15'}).text
+    gabungan_artikel = []
+    for i in range(len(artikel1)):
+        gabungan_artikel.append(artikel1[i].text.strip())
+    content = ' '.join(gabungan_artikel)
+    return save_berita(judul, content, url)
+
+
+if __name__ == '__main__':
+    app.run()
